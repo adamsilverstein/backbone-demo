9ad6f680500025f4a0a1dc0ca050320b57f7ff2b
Adam Silverstein <adam@10up.com>
2015-03-15 14:28:27 -0600

    Localize some data, add inline docs

--- functions.php
+++ functions.php
@@ -9,14 +9,46 @@ namespace backbonedemo;
 
 const VERSION = 0.1;
 
+function bbdemo_include_scripts() {
 
+	/**
+	 * If the URL matches '/demo1' include Demo1 code
+	 */
 	if ( false !== strpos( stripslashes( $_SERVER['REQUEST_URI'] ), '/demo1' ) ) {
+
+		/**
+		 * Enqueue the demo1 JavaScript.
+		 */
 		wp_enqueue_script(
-			'backbonedemo',
-			get_theme_root_uri( 'js/backbonedemo.js' ),
-			array( 'backbone' ),
-			VERSION,
-			true
+			'backbonedemo',                                        /* Script handle */
+			get_template_directory_uri() . '/js/backbonedemo1.js', /* Script path */
+			array( 'wp-backbone' ),                                /* Require WP Backbone */
+			VERSION,                                               /* Version caching */
+			false                                                  /* Leave in header */
 		);
-		include 'demo1/demo1.php';
+
+		/**
+		 * Bootstrap data for our application using wp_localize_script
+		 */
+		$demodata = array(
+				'apiurl' => get_json_url(), /* Pass the WP REST API url */
+			);
+
+		wp_localize_script(
+			'backbonedemo', /* The script handle */
+			'demodata',     /* The localized variable name */
+			$demodata       /* The data to pass */
+		);
 	}
+}
+
+
+add_action( 'wp_enqueue_scripts', 'backbonedemo\bbdemo_include_scripts' );
+	/**
+	 * If the URL matches '/demo1' include Demo1
+	 */
+	add_filter( 'the_content', function() {
+			if ( false !== strpos( stripslashes( $_SERVER['REQUEST_URI'] ), '/demo1' ) ) {
+				include 'demo1/demo1.php';
+			}
+		} );
